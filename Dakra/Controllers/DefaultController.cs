837b3eccc657ab7ddfa23421c5705c3fbf97d772
jastrzab4 <artur.e.dabrowski@gmail.com>
2016-06-02 14:43:44 +0200

    Add files via upload

---
+++ Dakra/Controllers/DefaultController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
+using Dakra.Models;
+using Dakra.Models.Default;
+using KRD.RestApi;
+using KRD.RestApi.TeamCity;
+using KRD.RestApi.TeamCity.Entity;
+
+
+namespace Dakra.Controllers
+{
+  public class DefaultController : Controller
+  {
+    private readonly TeamCityClient _teamCityClient;
+    private readonly List<CodeCoverageModel> _codeCoverageList = new List<CodeCoverageModel>();
+
+    public DefaultController()
+    {
+      _teamCityClient = TeamCityClient.Create("teamcity", 90, new LoginData("guest", "guest"));
+    }
+
+    // GET: Default
+    public ActionResult Index()
+    {
+      var projectsList = _teamCityClient.Projects.GetAll().ToList();
+
+      return View(projectsList);
+    }
+
+    public ActionResult ProjectParameters(string projectId)
+    {
+      ProjectDetails projectDetails = _teamCityClient.Projects.GetProjectDetailsById(projectId);
+
+      var codeCoverageModel = new CodeCoverageModel()
+      {
+        ProjectName = projectDetails.Name,
+        Branches = projectDetails.ConfigurationsList,
+        Before = string.Empty,
+        After = string.Empty
+      };
+
+      return View("Statistics", codeCoverageModel);
+    }
+
+    public ActionResult ShowBuilds(CodeCoverageModel codeCoverageModel)
+    {
+      // codeCoverageModel.SelectedBranch = projectConfiguration;
+      codeCoverageModel.BuildsCollection = 
+        _teamCityClient
+        .Projects.BuildTypes.Builds
+        .GetBuildsCollection(codeCoverageModel.ProjectName, codeCoverageModel.SelectedBranch.Name, BuildStatus.SUCCESS, null, 2, 1);
+
+      return View("Statistics", codeCoverageModel);
+    }
+
+    public ActionResult ShowStatistics(CodeCoverageModel codeCoverageModel)
+    {
+      var statisticsBefore = _teamCityClient.Projects.BuildTypes.Builds.GetBuildStatisticsById(codeCoverageModel.SelectedBuildBeforeId);
+      var statisticsAfter = _teamCityClient.Projects.BuildTypes.Builds.GetBuildStatisticsById(codeCoverageModel.SelectedBuildAfterId);
+      
+      if (statisticsBefore != null)
+      {
+        codeCoverageModel.Before = statisticsBefore.Property.Where(x => x.Name == "CodeCoverageS").Select(y => y.Value).FirstOrDefault();
+      }
+
+      if (statisticsAfter != null)
+      {
+        codeCoverageModel.After = statisticsAfter.Property.Where(x => x.Name == "CodeCoverageS").Select(y => y.Value).FirstOrDefault();
+      }
+
+      return View("Statistics", codeCoverageModel);
+    }
+  }
+}
---
+++ Dakra/Models/Default/CodeCoverageModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KRD.RestApi.TeamCity.Entity;
+
+namespace Dakra.Models.Default
+{
+  public class CodeCoverageModel
+  {
+    public string ProjectName { get; set; }
+
+    public ProjectConfigurationsList Branches { get; set; } 
+
+    public ProjectConfiguration SelectedBranch { get; set; }
+
+    public string SelectedBuildBeforeId { get; set; }
+
+    public string SelectedBuildAfterId { get; set; }
+
+    public BuildsCollection BuildsCollection { get; set; }
+
+    public string Before { get; set; }
+
+    public string After { get; set; }
+  }
+}
